eb9c1d642309720403a97441711eb151223b793c
Hector Soto <hector_17rock@hotmail.com>
2025-03-26 18:02:21 -0400

    Commit changes

--- print_functions.c
+++ print_functions.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 int print_char(va_list args)
 {
 	return write(1, va_arg(args, int *), 1);
@@ -9,41 +11,42 @@ int print_string(va_list args)
 	char *str = va_arg(args, char *);
 	if (str == NULL)
 		str = "(null)";
-	return write(1, str, strlen(str));
+	write(1, str, strlen(str));
+	return (0);
 }
 
 /* Function to print an integer */
-int print_int(va_list args)
+int print_int(va_list args);
 {
 	return (0);
 }
 
 /* Function to print an unsigned integer */
-int print_unsigned(va_list args)
+int print_unsigned(va_list args);
 {
 	return (0);
 }
 
 /* Function to print in octal */
-int print_octal(va_list args)
+int print_octal(va_list args);
 {
 	return (0);
 }
 
 /* Function to print hexadecimal */
-int print_hex(va_list args)
+int print_hex(va_list args);
 {
 	return (0);
 }
 
 /* Function to print a pointer address */
-int print_address(va_list args)
+int print_address(va_list args);
 {
 	return (0);
 }
 
 /* Function to print the percent sign */
-int print_percent(va_list args)
+int print_percent(va_list args);
 {
 	(void)args;
 	return write(1, "%", 1);
