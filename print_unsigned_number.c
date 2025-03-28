704e27eca2b30ffef644247e2568cad35e94f703
Hector Soto <hector_17rock@hotmail.com>
2025-03-28 14:18:06 -0400

    Commit changes

--- print_unsigned_number.c
+++ print_unsigned_number.c
@@ -9,23 +9,23 @@
  */
 int print_unsigned_number(va_list args)
 {
-    unsigned int n;
-    int count;
-    char digit;
-    va_list args_copy;
+	unsigned int n;
+	int count;
+	char digit;
+	va_list args_copy;
 
-    va_copy(args_copy, args);
-    n = va_arg(args, unsigned int);
-    count = 0;
+	va_copy(args_copy, args);
+	n = va_arg(args, unsigned int);
+	count = 0;
 
-    if (n / 10)
-    {
-        va_end(args_copy);
-        count += print_unsigned_number(args_copy);
-    }
+	if (n / 10)
+	{
+		va_end(args_copy);
+		count += print_unsigned_number(args_copy);
+	}
 
-    digit = (n % 10) + '0';
-    count += write(1, &digit, 1);
+	digit = (n % 10) + '0';
+	count += write(1, &digit, 1);
 
-    return (count);
+	return (count);
 }
